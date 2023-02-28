f6a5943b4b201ef4e894234247354fe59e9a6040
Hemantkumar Lashkar <hemantlashkar11nov@gmail.com>
2023-02-28 13:38:41 +0530

    This is the second commit

--- sum.cc
+++ sum.cc
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"This is the sum of the number is:";
+    cout<<"This is the diffrence of the number is:";
     int a=2;
     int b=34;
     int c=a+b;
